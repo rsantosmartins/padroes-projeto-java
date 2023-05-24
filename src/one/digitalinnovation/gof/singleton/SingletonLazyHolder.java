5dcdd8b05fe69c8578f5bbb23d7f1e9921f9c9e9
rsantosmartins <rsantosmartins@icloud.com>
2023-05-24 15:23:04 -0300

    first commit

---
+++ src/one/digitalinnovation/gof/singleton/SingletonLazyHolder.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation.gof.singleton;
+/**
+ * Singleton "Lazy Holder".
+ * 
+ * @author rsantosmartins
+ *
+ */
+public class SingletonLazyHolder {
+	
+	private static class InstaceHolder {
+		public static SingletonLazyHolder  instancia = new SingletonLazyHolder();
+	}
+	private SingletonLazyHolder() {
+		super();
+	}
+	
+	public static SingletonLazyHolder getInstancia() {
+		return InstaceHolder.instancia;
+	}
+
+}
